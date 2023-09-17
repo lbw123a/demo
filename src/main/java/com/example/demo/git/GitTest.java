88231e5e16d3c96e6db82123ae729d084e120752
lbw <2121622891@qq.com>
2023-09-17 20:20:49 +0800

    first commit

--- src/main/java/com/example/demo/git/GitTest.java
+++ src/main/java/com/example/demo/git/GitTest.java
@@ -3,7 +3,6 @@ package com.example.demo.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello, git!");
-        System.out.println("hello, git2!");
-        System.out.println("hello, git3!");
+
     }
 }
